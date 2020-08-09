80c604bb47175e7b922e6ce01ce54549ca06419d
Chris Spurgeon <chris@spurgeonworld.com>
2020-08-08 23:17:28 -0700

    dynamic photo

--- site/scripts/bvb.js
+++ site/scripts/bvb.js
@@ -14,8 +14,30 @@ $("#bird2").on("change", function() {
     $("#bird1").removeClass("hide");
 });
 
-
-
+$.get("scripts/FRGU.json", function(data) {
+    window.FRGU = data;
+    window.FRGU.photoCount = data.photos.length;
+    window.FRGU.next = 0;
+    console.log('got json');
+});
+$.get("scripts/LAGU.json", function(data) {
+    window.LAGU = data;
+    window.LAGU.photoCount = data.photos.length;
+    window.LAGU.next = 0;
+    console.log('got json');
+});
+$(".testphoto").click(function() {
+    var which;
+    if (Math.random() < 0.5) {
+        which = FRGU;
+    } else {
+        which = LAGU;
+    }
+    var $img = $('.testphoto')[0];
+    $($img).attr('src', which.photos[which.next]);
+    which.next++;
+    console.log('which.next is ' + which.next);
+});
 
 
 
