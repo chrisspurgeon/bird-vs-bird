a99f1691f50d31a04d5cc3991f06f0fb9065cd17
Chris Spurgeon <chris@spurgeonworld.com>
2020-08-14 14:35:11 -0700

    Built out chooser and choice title section.

--- site/scripts/bvb.js
+++ site/scripts/bvb.js
@@ -1,18 +1,68 @@
+var bird1Category = '';
+var bird1Value = '';
+var bird1Name = '';
+var bird2Category = '';
+var bird2Value = '';
+var bird2Name = '';
+var $battleTitle = $('.battleTitle');
+var $leftIntroImage = $('.intro-image-left');
+var $rightIntroImage = $('.intro-image-right');
+
 $("#bird1").on("change", function() {
-    console.log("change 1 triggered");
+    bird1Value = $("#bird1").val();
+    bird1Category = $("#bird1 option:selected").data('category');
+    bird1Name = $("#bird1 option:selected").text();
+    console.log("change 1 triggered with a value of " + bird1Value + ', which has a category of ' + bird1Category);
     $("#bird1").addClass("hide");
+    $('.js-startover').removeClass('hide');
+
+    var bird2Options = $("#bird2 option");
+    $(bird2Options).removeClass('hide');
+    for (var i = 0; i < bird2Options.length; i++) {
+        if ($(bird2Options[i]).data('category') !== bird1Category || $(bird2Options[i]).val() === bird1Value) {
+            $(bird2Options[i]).addClass('hide');
+        }
+    }
     $("#bird2").val('');
     $("#bird2").focus();
     $("#bird2").removeClass("hide");
+    console.log()
 });
 
 $("#bird2").on("change", function() {
     console.log("change 2 triggered");
+    bird2Value = $("#bird2").val();
+    bird2Category = $("#bird2 option:selected").data('category');
+    bird2Name = $("#bird2 option:selected").text();
     $("#bird2").addClass("hide");
-    $("#bird1").val('')
-    $("#bird1").focus();
-    $("#bird1").removeClass("hide");
+    displayIntro();
 });
+
+$('.js-startover').on('click', resetAll);
+
+function resetAll() {
+    console.log('fired reset all');
+    $('#bird2').addClass('hide');
+    $('#bird1 option').removeClass('hide');
+    $('#bird2 option').removeClass('hide');
+    $('#bird1').val('');
+    $('#bird1').removeClass('hide');
+    $('.js-startover').addClass('hide');
+    $('.intro-images').addClass('hide');
+    $battleTitle.addClass('hide');
+    bird1Category = '';
+    bird1Value = '';
+    bird2Category = '';
+    bird2Value = '';
+}
+
+function displayIntro() {
+    $battleTitle.html(bird1Name + ' vs. ' + bird2Name);
+    $battleTitle.removeClass('hide');
+    $leftIntroImage.attr('src', 'images/' + bird1Value + '-left.jpg');
+    $rightIntroImage.attr('src', 'images/' + bird2Value + '-right.jpg');
+    $('.intro-images').removeClass('hide');
+}
 
 $.get("scripts/FRGU.json", function(data) {
     window.FRGU = data;
